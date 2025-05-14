5b116d1bdacb6795c20fe33c8ea120d2551260d7
rahulaauji-30 <rahulaauji71@gmail.com>
2025-05-14 06:01:07 +0000

    Implemented delete in bst

--- Trees/Linked List/binarysearchtree.py
+++ Trees/Linked List/binarysearchtree.py
@@ -1,3 +1,6 @@
+from ctypes.wintypes import tagMSG
+
+
 class Node:
     def __init__(self,val,left=None,right=None):
         self.val = val
@@ -34,3 +37,31 @@ class BinarySearchTree:
         self.postorder(root.left)
         self.postorder(root.right)
         print(root.val, end=" ")
+
+    def inorder_successor(self,root):
+         current = root
+         while current and current.left:
+             current = current.left
+         return current
+
+
+    def delete(self, root, data):
+        if root is None:
+            return None
+
+        if root.val > data:
+            root.left =  self.delete(root.left,data)
+        elif root.val < data:
+            root.right = self.delete(root.right,data)
+        else:
+            if root.left is None and root.right is None:
+                return None
+            elif root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            else:
+                temp = self.inorder_successor(root.right)
+                root.val = temp.val
+                root.right = self.delete(root.right, temp.val)
+        return root
